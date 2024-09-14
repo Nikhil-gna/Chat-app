679d359dd60afb6088d45cc2a24c6c6d9ed44247
Nikhil Anand <102014379+Nikhil-gna@users.noreply.github.com>
2024-09-14 12:17:18 +0530

    added redis and socket.io

---
+++ apps/server/src/services/socket.ts
@@ -0,0 +1,55 @@
+import { Server } from "socket.io";
+import Redis from "ioredis";
+
+const pub = new Redis({
+  host: process.env.REDIS_HOST,
+  port: 13738,
+  username: process.env.REDIS_USERNAME,
+  password: process.env.REDIS_PASS,
+});
+const sub = new Redis({
+  host: process.env.REDIS_HOST,
+  port: 13738,
+  username: process.env.REDIS_USERNAME,
+  password: process.env.REDIS_PASS,
+});
+
+class SocketService {
+  private _io: Server;
+
+  constructor() {
+    console.log("Init Socket Service...");
+    this._io = new Server({
+      cors: {
+        allowedHeaders: ["*"],
+        origin: "*",
+      },
+    });
+    sub.subscribe("MESSAGES");
+  }
+
+  public initListeners() {
+    const io = this.io;
+    console.log("Init Socket Listners....");
+
+    io.on("connect", (socket) => {
+      console.log(`New Socket Connected`, socket.id);
+      socket.on("event:message", async ({ message }: { message: string }) => {
+        console.log("New Message Rec.", message);
+
+        //publish this message to redis
+        await pub.publish("MESSAGES", JSON.stringify({ message }));
+      });
+    });
+    sub.on("message", (channel, message) => {
+      if (channel === "MESSAGES") {
+        io.emit("message", message);
+      }
+    });
+  }
+  get io() {
+    return this._io;
+  }
+}
+
+export default SocketService;
